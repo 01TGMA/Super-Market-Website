e15d8846b5f7867b31247aff406bf7efd1ea5a01
01TGMA <Ghostmanhades@gmail.com>
2025-03-18 01:55:09 +0100

    Hamburger Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const  hamMenu = document.querySelector('.ham-menu');
+
+const OffScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener('click',() =>{
+    hamMenu.classList.toggle('active')
+    OffScreenMenu.classList.toggle('active')
+})
